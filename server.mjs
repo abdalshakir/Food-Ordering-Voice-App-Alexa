45b32cc5da12cce40e60c9d7fa2611f9f19d4014
Abdal Shakir <64357110+abdalshakir@users.noreply.github.com>
2022-01-20 11:52:01 +0500

    testing

--- server.mjs
+++ server.mjs
@@ -1,90 +1,96 @@
 import express from "express";
-import Alexa, { SkillBuilders } from 'ask-sdk-core';
+import Alexa, { SkillBuilders } from "ask-sdk-core";
 import morgan from "morgan";
-import { ExpressAdapter } from 'ask-sdk-express-adapter';
-import mongoose from 'mongoose';
-import axios from "axios";
+import { ExpressAdapter } from "ask-sdk-express-adapter";
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
 
-mongoose.connect('mongodb+srv://abdalshakir:mainnahibataunga715715@cluster0.6i9n6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect(
+  `mongodb+srv://abdalshakir:mainnahibataunga715715@cluster0.6i9n6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+);
 
-const schema = new mongoose.Schema({
-    skillName: String,
-    clientName: String,
-    createdOn: { type: Date, default: Date.now },
+const usageSchema = new Schema({
+  skillName: String,
+  clientName: String,
+  createdOn: { type: Date, default: Date.now },
 });
 
-const Usage = mongoose.model('Usage', schema);
+const Usage = mongoose.model("Usage", usageSchema);
 
 const app = express();
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 const PORT = process.env.PORT || 3000;
 
-
 const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        const speakOutput = 'Fallback intent: Sorry, I had trouble doing what you asked. Please try again.';
-        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    const speakOutput =
+      "Fallback intent: Sorry, I had trouble doing what you asked. Please try again.";
+    console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
 };
 
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Welcome to my food ordering app';
-        const reprompt = 'I am your virtual assistant. You can ask for the menu';
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+    );
+  },
+  handle(handlerInput) {
+      var newUsage = new Usage({
+          skillName: 'Food ordering skill',
+          clientName: 'Abdal Shakir'
+      }).save();
+      
+    const speakOutput = "Welcome to my food ordering app";
+    const reprompt = "I am your virtual assistant. You can ask for the menu";
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(reprompt)
-            .withSimpleCard("Kababjees", speakOutput)
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(reprompt)
+      .withSimpleCard("Kababjees", speakOutput)
+      .getResponse();
+  },
 };
 
 const IntroHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'Intro';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello';
-        const reprompt = 'Hello';
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "Intro"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = "Hello";
+    const reprompt = "Hello";
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(reprompt)
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(reprompt)
+      .getResponse();
+  },
 };
 
 const skillBuilder = SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        IntroHandler,
-    )
-    .addErrorHandlers(
-        ErrorHandler
-    )
+  .addRequestHandlers(LaunchRequestHandler, IntroHandler)
+  .addErrorHandlers(ErrorHandler);
 const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, false, false);
 
-app.post('/api/v1/webhook-alexa', adapter.getRequestHandlers());
+app.post("/api/v1/webhook-alexa", adapter.getRequestHandlers());
 
-app.use(express.json())
-app.get('/profile', (req, res, next) => {
-    res.send("This is my profile");
+app.use(express.json());
+app.get("/profile", (req, res, next) => {
+  res.send("This is my profile");
 });
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}`);
 });
